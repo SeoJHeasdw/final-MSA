6699c37f2682565fb77bccbdac992391d42f9e4a
SeoJHeasdw SEO <tjwpghangel@naver.com>
2024-02-21 08:44:48 +0000

    preRemove

--- order/src/main/java/airobotrental/domain/Order.java
+++ order/src/main/java/airobotrental/domain/Order.java
@@ -33,11 +33,15 @@ public class Order {
         OrderModifed orderModifed = new OrderModifed(this);
         orderModifed.publishAfterCommit();
 
-        OrderCancel orderCancel = new OrderCancel(this);
-        orderCancel.publishAfterCommit();
-
         OrderPaused orderPaused = new OrderPaused(this);
         orderPaused.publishAfterCommit();
+    }
+
+    @PreRemove
+    public void onPreRemove() {
+        OrderCancel orderCancel = new OrderCancel(this);
+        orderCancel.publishAfterCommit();
+    
     }
 
     public static OrderRepository repository() {
@@ -49,120 +53,27 @@ public class Order {
 
     //<<< Clean Arch / Port Method
     public static void notiyForUser(PaymentPaused paymentPaused) {
-        //implement business logic here:
-
-        /** Example 1:  new item 
-        Order order = new Order();
-        repository().save(order);
-
-        */
-
-        /** Example 2:  finding and process
-        
-        repository().findById(paymentPaused.get???()).ifPresent(order->{
-            
-            order // do something
-            repository().save(order);
-
-
-         });
-        */
+        // notiy
 
     }
 
-    //>>> Clean Arch / Port Method
-    //<<< Clean Arch / Port Method
     public static void notiyForUser(PaymentStarted paymentStarted) {
-        //implement business logic here:
-
-        /** Example 1:  new item 
-        Order order = new Order();
-        repository().save(order);
-
-        */
-
-        /** Example 2:  finding and process
-        
-        repository().findById(paymentStarted.get???()).ifPresent(order->{
-            
-            order // do something
-            repository().save(order);
-
-
-         });
-        */
+        // notiy
 
     }
 
-    //>>> Clean Arch / Port Method
-    //<<< Clean Arch / Port Method
     public static void notiyForUser(PaymentStopped paymentStopped) {
-        //implement business logic here:
-
-        /** Example 1:  new item 
-        Order order = new Order();
-        repository().save(order);
-
-        */
-
-        /** Example 2:  finding and process
-        
-        repository().findById(paymentStopped.get???()).ifPresent(order->{
-            
-            order // do something
-            repository().save(order);
-
-
-         });
-        */
-
+        // notiy
     }
 
-    //>>> Clean Arch / Port Method
-    //<<< Clean Arch / Port Method
+    
     public static void notiyForUser(InstallStarted installStarted) {
-        //implement business logic here:
-
-        /** Example 1:  new item 
-        Order order = new Order();
-        repository().save(order);
-
-        */
-
-        /** Example 2:  finding and process
-        
-        repository().findById(installStarted.get???()).ifPresent(order->{
-            
-            order // do something
-            repository().save(order);
-
-
-         });
-        */
+        // notiy
 
     }
 
-    //>>> Clean Arch / Port Method
-    //<<< Clean Arch / Port Method
     public static void notiyForUser(UninstallStarted uninstallStarted) {
-        //implement business logic here:
-
-        /** Example 1:  new item 
-        Order order = new Order();
-        repository().save(order);
-
-        */
-
-        /** Example 2:  finding and process
-        
-        repository().findById(uninstallStarted.get???()).ifPresent(order->{
-            
-            order // do something
-            repository().save(order);
-
-
-         });
-        */
+        // notiy
 
     }
     //>>> Clean Arch / Port Method
